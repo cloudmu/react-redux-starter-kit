5296d6c4e44699f89099e3f3fc804530a7599a2b
Rich Brown <richb.hanover@gmail.com>
2016-08-28 11:31:49 -0400

    <Timebar /> now receives updates from server via socket.io

--- src/components/timebar/timebar.js
+++ src/components/timebar/timebar.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 import './timebar.css';
+import io from 'socket.io-client';
 
 export default class Timebar extends Component {
-   render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: 'A while ago, the time was: Wed Aug 24 2016 08:17155 GMT-0400 (EDT)'
+    };
+    this.socket = io();                             // set up socket to the server
+    this.socket.on('time', (msg) => {               // when the server emits 'time' message
+      console.log(`socket received: ${msg.text}`);  // log the new time
+      this.setState({ time: msg.text });            // set the state to force a re-render
+    })
+  }
+  render() {
     return (
         <div className="panel panel-custom">
           <div className="panel-body text-xs-center">
-            This could be the time
+            {this.state.time}
           </div>
         </div>
       )
