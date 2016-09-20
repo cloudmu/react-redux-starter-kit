541f749e6de1815e8c4f077351e0873f58e392fe
cloudmu <yunjun.mu@gmail.com>
2016-09-20 12:09:15 -0400

    Add favicon and 404.gif

--- src/containers/misc/NotFound.js
+++ src/containers/misc/NotFound.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import gif404 from './404.gif';
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div className="container">
+      <div style={{margin: '0 auto', width: '400px'}}>
         <h1>404!</h1>
         <p>Oops, no such page exists.</p>
+        <img src={gif404} alt='404 gif'/>
       </div>
     );
   }
